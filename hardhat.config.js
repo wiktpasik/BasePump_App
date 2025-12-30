4d90208a5bfd8f7cbee0f5b165a1c57d15ecfe62
wiktpasik <xifla126@LAPTOP-8S23IQ8C.localdomain>
2025-12-30 15:32:47 +0100

    Update config for Base Sepolia deployment + add .gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.27", // lub jaka jest w repo
+  networks: {
+    baseSepolia: {
+      url: "https://base-sepolia-rpc.publicnode.com",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 84532,
+    },
+    base: {
+      url: "https://base-rpc.publicnode.com",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 8453,
+    },
+  },
 };
